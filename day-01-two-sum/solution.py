183032f38b679af8f6dad47a6d9f3dc9a36ac32c
D Tharuneswar <dtharuneswar@aditya.ac.in>
2026-02-25 08:30:03 +0530

    Day 1

--- day-01-two-sum/solution.py
+++ day-01-two-sum/solution.py
@@ -35,4 +35,5 @@ if __name__ == "__main__":
     assert two_sum([2, 7, 11, 15], 9) == [0, 1]
     assert two_sum([3, 2, 4], 6) == [1, 2]
     assert two_sum([3, 3], 6) == [0, 1]
+    assert two_sum([1, 2, 3], 7) == []
     print("All test cases passed!")
